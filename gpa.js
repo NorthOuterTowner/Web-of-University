88cd50354186c2f03e1fc700276fb1b2cd9bc420
NorthOuterTowner <153065876+NorthOuterTowner@users.noreply.github.com>
2024-02-02 15:28:16 +0800

    Update gpa.js

--- gpa.js
+++ gpa.js
@@ -1,16 +1,22 @@
 console.log("generate table has been submit.");
+
+/*get numbers of curriculum*/
 var number = parseInt(document.getElementById("number").value);
+
 for (var i = 0; i < number; i++) {
+    /*Score&Credit Legend*/
     var fieldset = document.createElement("fieldset");
     var legend = document.createElement("legend");
     legend.appendChild(document.createTextNode(i+1+" curriculum"));
     fieldset.appendChild(legend);
-    
+
+    /*Score Label*/
     var label1 = document.createElement("label");
     label1.htmlFor = "score" + i;
     label1.appendChild(document.createTextNode("Score:"));
     fieldset.appendChild(label1);
-    
+
+    /*Score Input*/
     var input1 = document.createElement("input");
     input1.type = "number";
     input1.id = "score" + i;
@@ -20,11 +26,13 @@ for (var i = 0; i < number; i++) {
     input1.max = 100;
     fieldset.appendChild(input1);
 
+    /*Credit Label*/
     var label2 = document.createElement("label");
     label2.htmlFor = "credit" + i;
     label2.appendChild(document.createTextNode("Credit:"));
     fieldset.appendChild(label2);
-    
+
+    /*Credit Input*/
     var input2 = document.createElement("input");
     input2.type = "number";
     input2.id = "credit" + i;
@@ -35,9 +43,11 @@ for (var i = 0; i < number; i++) {
     fieldset.appendChild(input2);
     document.getElementById("formContainer").appendChild(fieldset);
 }
+
 console.log("table has been generated.");
 
 be_gray();
+
 var gpaForm=document.getElementById("myForm");
 empty(gpaForm);
 
@@ -47,6 +57,7 @@ calculateBtn.type="button";
 calculateBtn.value="CalculateGPA";
 startCal.appendChild(calculateBtn);
 
+/*Submit the data of Scores and credits*/
 calculateBtn.onclick=function(){
     var scoreAll=new Array(number);
     var valueAll=new Array(number);
